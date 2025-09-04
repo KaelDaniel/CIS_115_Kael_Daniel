fc66bf4631709c012df5137710c8fc9e104a7e21
SANDHILLS\lh178-18 <jad.talbert@gmail.com>
2025-09-04 09:47:48 -0400

    finished calculator

--- calculator.py
+++ calculator.py
@@ -7,3 +7,13 @@ num3=4
 num4=400
 solve2=int(num4)/int(num3)
 print(solve2)
+
+num5=2
+num6=10
+solve3=int(num5)*int(num6)
+print(solve3)
+
+num7=10
+num8=9.81**2
+solve4=float(num7)*float(num8)
+print(solve4)
