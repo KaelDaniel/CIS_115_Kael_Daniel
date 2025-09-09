8b9accc941df68e0ef7c67f12759ca9fa1e17b4f
Kael Daniel <danielko552@mail.sandhills.edu>
2025-09-09 09:40:29 -0400

    Create Mathematical_operation.py

---
+++ Mathematical_operation.py
@@ -0,0 +1,8 @@
+num1 = input("number 1: ")
+num2 = input("number 2: ")
+num3 = input("number 3: ")
+
+solve = (float(num1) + float(num2))/float(num3)
+
+if solve > float(0): print("The mathematical operation is > 0")
+else: print("The mathematical operation is <= 0")
