02bf5bff3cf4ed650d8e340472d619e97283cedf
SANDHILLS\LH218-26 <jad.talbert@gmail.com>
2025-09-10 10:20:39 -0400

    fixed some things, and made even or odd

---
+++ Even_or_Odd.py
@@ -0,0 +1,6 @@
+num1=input("number1: ")
+num2=input("number2: ")
+
+Modulo = int(num1) % int(num2)
+if Modulo == 0: print("The number is even")
+else: print("The number is odd")
--- Invalid.py
+++ Invalid.py
@@ -4,4 +4,4 @@ num2 = input("number 2: ")
 Subtract = int(num1) - int(num2)
 
 if Subtract < 0: print("Invalid! The value is less than 0")
-else: print("the subtraction is: ", Subtract)
+else: print("the values entered were valid intergers")
