2e88e223ab75d7f265890d943499f7ce2c23cb03
SANDHILLS\LH218-26 <jad.talbert@gmail.com>
2025-09-10 10:13:38 -0400

    made invalid

---
+++ Invalid.py
@@ -0,0 +1,7 @@
+num1 = input("number 1: ")
+num2 = input("number 2: ")
+
+Subtract = int(num1) - int(num2)
+
+if Subtract < 0: print("Invalid! The value is less than 0")
+else: print("the subtraction is: ", Subtract)
