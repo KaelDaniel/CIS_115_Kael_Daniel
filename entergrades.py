086e198586a9f44b06af44d8230ac2f7ae33a0bd
SANDHILLS\LH218-26 <jad.talbert@gmail.com>
2025-09-15 10:49:02 -0400

    made progress on grade

--- entergrades.py
+++ entergrades.py
@@ -1,1 +1,4 @@
-#this program will enter ten grades that the user inputs, and then 
+#this program will prompt the user to enter how many grades they want to enter, and then prompt them to enter what the grades are
+# it will then print out how many grades they entered
+
+howmanygrades = int(input("How many grades do you want to enter? ")) 
