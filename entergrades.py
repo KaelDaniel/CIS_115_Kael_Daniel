0a1aa34fc010858ceeec90d713938ff05864d9da
jadTalbert <talbertj@sandhills.edu.com>
2025-09-16 10:19:53 -0400

    made progrees, will need help

--- entergrades.py
+++ entergrades.py
@@ -1,4 +1,9 @@
 #this program will prompt the user to enter how many grades they want to enter, and then prompt them to enter what the grades are
 # it will then print out how many grades they entered
 
-howmanygrades = int(input("How many grades do you want to enter? ")) 
+howmanygrades = input("Would you like to enter your Grade(s)? (type y for yes): ")
+
+while howmanygrades == 'y':
+    grade = input("what is the grade: ")
+
+    howmanygrades = input("would you like to input another grade? (type y for yes): ")
