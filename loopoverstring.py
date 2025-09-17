5c9cce1b8358737bde5e79054b4b8358e4617285
SANDHILLS\LH218-26 <jad.talbert@gmail.com>
2025-09-17 10:21:08 -0400

    made and completed loop over string

---
+++ loopoverstring.py
@@ -0,0 +1,6 @@
+#this program will use a for-in loop to loop over a string and print out the letters in the string
+
+myVar = "do or do not, there is no try"
+
+for letter in myVar:
+    print(letter)
