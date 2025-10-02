19ea5a16446cd0a9cd21a4792af25177c55293b6
jadTalbert <talbertj@sandhills.edu>
2025-10-02 09:52:55 -0400

    Finished it

--- kinetic_energy.py
+++ kinetic_energy.py
@@ -1,11 +1,10 @@
 #this program will calculate the kinetic energy of a moving object
 
-import math
-
 mass = int(input("give the mas of an object: "))
 velocity = int(input("give a velocity of an object: "))
 
 def kinetic_energy(mass,velocity):
-    return math((mass*0.5)*(velocity**2))
+    ke= (mass*0.5)*(velocity**2)
+    return ke
 
 print(kinetic_energy(mass,velocity))
