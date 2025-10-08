6e061bb9b050dcc83bb872521cdb6faa6b196f67
jadTalbert <talbertj@sandhills.edu>
2025-10-08 11:17:17 -0400

    this was fortunatly easy

--- slice_list.py
+++ slice_list.py
@@ -1,2 +1,7 @@
 #this program will slice a list
 months=["jan","feb","march","april","may","june","july","august","sept","oct","nov","dec"]
+
+def slice_list(month):
+    return month[4:6]
+
+print(slice_list(months))
