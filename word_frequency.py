3bb063c91a0d26e01e8e71d5674c4bea59427794
jadTalbert <talbertj@sandhills.edu>
2025-10-21 09:39:39 -0400

    working on

---
+++ word_frequency.py
@@ -0,0 +1,10 @@
+#This program cuts a sentence and then counts the frequency of each word in the sentence.
+
+yeet = input("Please enter a sentence: ")
+
+def word_frequency(sentence):
+    words = sentence.split()
+    frequency = {}
+    for word in words:
+
+word_frequency(yeet)
