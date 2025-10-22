a89004492e5768d121d6bb8a3836a22d76e14304
SANDHILLS\LH218-26 <jad.talbert@gmail.com>
2025-10-22 10:28:33 -0400

    finished and made it

--- word_frequency.py
+++ word_frequency.py
@@ -1,10 +1,17 @@
 #This program cuts a sentence and then counts the frequency of each word in the sentence.
 
-yeet = input("Please enter a sentence: ")
+yeet = input("Please enter a sentence: ") #user input a sentence
 
-def word_frequency(sentence):
-    words = sentence.split()
-    frequency = {}
-    for word in words:
+frequency = {} #dictionary to hold word frequency
 
-word_frequency(yeet)
+def word_frequency(sentence): #function to count word frequency
+    words = sentence.split() #split the sentence into words
+    for word in words: #for loop to count each word
+        if word in frequency: #if word is already in dictionary, increment its count
+            frequency[word] += 1
+        else: #if word is not in dictionary, add it with count 1
+            frequency[word] = 1
+
+word_frequency(yeet) #call the function with user input before printing the frequency so the dictionary is populated
+
+print(frequency)
