3d119f40691fe6ed8072aab51ad895f09c3f4c75
jadTalbert <talbertj@sandhills.edu>
2025-11-04 10:18:35 -0500

    Started it and made the begining

---
+++ Final_Project.py
@@ -0,0 +1,34 @@
+#the final Project
+
+def Print_box():
+    print("        ---------------------------------------------------------------")
+    print("	                           PRODUCT CATALOG                     ")
+    print("	---------------------------------------------------------------")
+    print("	1      |    USB Drive(128 GB)                           |   $12.00")
+    print("	2      |   Mac Book Pro(15 inch)                    |   $2900.00")
+    print("	3      |   Arduino 1010(with blue tooth)     |   $48.00")
+    print("	4      |   Ring Camera(wireless)                    |   $156.00")
+    print("	5      |   Smart TV(TCL 70 inch)                     |   $359.00")
+    print("	----------------------------------------------------------------")
+
+Product_Catalog = {
+"1":12.00, 
+"2":2900.00, 
+"3":48.00, 
+"4":156.00, 
+"5":359.00, 
+}
+
+Print_box()
+
+Shopping_cart = {}
+User_info = {
+    "first name":v
+    "last name":w
+    "address":x
+    "city":y
+    "state":z
+    "zip/post code":a
+    "email":b
+    "phone":c
+}
